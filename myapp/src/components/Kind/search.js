68b21ab3c09a14694e72246329a80904f4e46b85
tongyouchun <1621955026@qq.com>
2019-06-28 08:33:07 +0800

    5

---
+++ myapp/src/components/Kind/search.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const SearchBar = () => (
+  <div className="SearchBar">
+    <form className= "for">
+      <input type = "text" placeholder="热门“AJ疯抢999元”" className= "text"/>
+      <input type = "button" value="消息" className= "button"/>
+    </form>
+  </div>
+)
+export default SearchBar;
--- myapp/src/components/cart/Cart.js
+++ myapp/src/components/cart/Cart.js
@@ -1,24 +1,31 @@
 import React from 'react';
-const Cart = () => (
-  <div className="Cart-content">
-    <h4>购物车</h4>
-    <ul className="Shopping">
-      <li className="te">
-        <div className="Shopping-top"><b></b><em></em><span>Tiffany & Co.</span><s>></s></div>
-        <div className="Shopping-bottom">
-          <b></b>
-          <span></span>
-          <s>
-            <p>系列925纯银心形钥匙迷你吊坠项链</p>
-            <ul>
-              <li></li>
-            </ul>
-            <span>¥ 2848</span>
-            <dl><dd>+</dd><b></b>1<dd>-</dd></dl>
-          </s>
+
+  class Cart extends React.Component {
+    render () {
+      return (
+        <div className="Cart-content">
+          <h4>购物车</h4>
+          <ul className="Shopping">
+            <li className="te">
+              <div className="Shopping-top"><b></b><em></em><span>Tiffany & Co.</span><s>></s></div>
+              <div className="Shopping-bottom">
+                <b></b>
+                <span className="span"></span>
+                <s>
+                  <p>系列925纯银心形钥匙迷你吊坠项链</p>
+                  <ul>
+                    <li>银色-16英寸</li>
+                  </ul>
+                  <dl><dt><em>¥</em> 2848</dt><dd><i>+</i><span>1</span><i>-</i></dd></dl>
+                </s>
+              </div>
+            </li>
+          </ul>
+          <div className="account">
+            <p><i></i><b>全选</b><s>¥2927</s></p><span>结算<em>(2)</em></span>
+          </div>
         </div>
-      </li>
-    </ul>
-  </div>
-)
+      )
+    }
+  }
 export default Cart;
